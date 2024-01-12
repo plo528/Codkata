9a066b35131d4200a8c0b2734906467ec93cd7df
kimjunseo <dpdl33000@naver.com>
2024-01-12 09:09:30 +0900

    2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+        if (num1 > 100 || num2 > 100)
+        {
+            Console.WriteLine("100이상은 입력불가합니다.");
+            return answer;
+        }
+        answer = num1 * num2;
+        return answer;
+    }
+}
